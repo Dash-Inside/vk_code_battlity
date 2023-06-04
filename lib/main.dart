eaf6e0e84118b4d8be009478acf70eb1ad5d0e51
(No) Zorro Positivo <pocket.red.fox@gmail.com>
2023-06-04 22:19:43 +0300

    feat: add VkBridge init

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:battlity/presentation/app.dart';
 import 'package:flutter/material.dart';
+import 'package:vk_bridge/vk_bridge.dart';
 
-void main() {
+void main() async {
+  await VKBridge.instance.init();
   runApp(const App());
 }
